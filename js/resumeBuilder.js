f11a616fe29f1ad8bf7d3b2e3c7b5e18415ba49a
Beverly Denese Nixon <beverlynixon@gmail.com>
2017-05-09 14:39:35 -0600

    Last Known Good Version

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,14 +2,8 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
  
- var name = "Beverly Nixon";
- var role = "Software Developer";
- var skills = ["Java", "Teaching" ];
- var formattedName = HTMLheaderName.replace("%data%", name);  
- var formattedRole = HTMLheaderRole.replace("%data%", role);
-
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
+ 
+ 
 
  var bio = {
 	"name" : "Beverly Nixon",
@@ -23,14 +17,16 @@ $("#header").prepend(formattedName);
 		
 	},
 	"welcomeMsg" : "Hello! How can I help you?",
-	//"skills" : ["Java", "Teaching", "Patience"],
+	"skills" : ["Programming", "Teaching", "Learning", "Patience"],
 	"bioPic" : "images/me.jpg"
 	
 	
 };
 
+
  var formattedName = HTMLheaderName.replace("%data%", bio.name);  
- var formattedRole = HTMLheaderRole.replace("%data%", bio.role); 
+ var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+
  var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);  
  var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github); 
  var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email); 
@@ -38,8 +34,10 @@ $("#header").prepend(formattedName);
  var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);  
  var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);  
  var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMsg);  
- //var formattedSkills = HTMLskills.replace(%data%, bio.skills)
  
+ 
+ $("#header").prepend(formattedRole);
+ $("#header").prepend(formattedName);
  
  $("#header").prepend(formattedBioPic);
  $("#header").append(formattedMobile);
@@ -49,14 +47,27 @@ $("#header").prepend(formattedName);
  $("#header").append(formattedLocation);
  
  $("#header").append(formattedWelcomeMsg);
- $("#header").append(HTMLskillsStart);
- //$("#header").append(formattedSkills);
+ if (bio.skills.length > 0){
+	$("#header").append(HTMLskillsStart);
+		var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
+		$("#skills").append(formattedSkill);
+		
+		formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
+		$("#skills").append(formattedSkill);
+		
+		formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
+		$("#skills").append(formattedSkill);
+		
+		formattedSkill = HTMLskills.replace("%data%", bio.skills[3]);
+		$("#skills").append(formattedSkill);
+		
+		}
  
  var work = {
 	"jobs" : [
 	{
 	 "employer": "Valencia College",
-	 "position":"Hands-on Math Cooridinator",
+	 "title":"Hands-on Math Cooridinator",
 	 "location": "Orlando, FL",
 	 "dates": "May 2015 - August 2016",
 	 "description": {
@@ -71,7 +82,7 @@ $("#header").prepend(formattedName);
 	},
 	{
 	 "employer": "Valencia College",
-	 "position":"Senior Curriculum Assistant",
+	 "title":"Senior Curriculum Assistant",
 	 "location": "Orlando, FL",
 	 "dates": "2009 - August 2016",
 	 "description": {
@@ -87,7 +98,7 @@ $("#header").prepend(formattedName);
 	
 	{
 	 "employer": "Valencia College",
-	 "position":"Adjunct Professor of Mathematics",
+	 "title":"Adjunct Professor of Mathematics",
 	 "location": "Orlando, FL",
 	 "dates": "2012",
 	 "description": {
@@ -101,7 +112,7 @@ $("#header").prepend(formattedName);
 	
 	{
 	 "employer": "Valencia College",
-	 "position":"Curriculum Assistant",
+	 "title":"Curriculum Assistant",
 	 "location": "Orlando, FL",
 	 "dates": "2008 - 2009",
 	 "description": {
@@ -110,7 +121,7 @@ $("#header").prepend(formattedName);
 			"mathematics students, Earned the Certificate in Tutor Training:",
 			"Advanced/Level II from the College Reading and Learning Association" 
 			]
-	 }
+			}
 
 	}
 	
@@ -118,6 +129,26 @@ $("#header").prepend(formattedName);
 	]
  }
  
+ 
+ for (job in work.jobs) {
+	 if (work.jobs.hasOwnProperty(job)) {
+		$("#workExperience").append(HTMLworkStart);
+	
+		var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+		var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+		var formattedEmployerTitle = formattedEmployer + formattedTitle;
+ 		$(".work-entry:last").append(formattedEmployerTitle);
+		
+		var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+		$(".work-entry:last").append(formattedDates);
+		
+		var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+		$(".work-entry:last").append(formattedLocation);
+		
+		var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+		$(".work-entry:last").append(formattedDescription);
+	 }
+ }
 
  
  var education = {
@@ -128,7 +159,7 @@ $("#header").prepend(formattedName);
 	 "degree": "AS",
 	 "major" : "Computer Programming and Analysis",
 	 "dates": "Dec 2016",
-	 "url":"valenciacollege.edu"
+	 "url":"http://valenciacollege.edu/"
 	 }, 
 	 
 	 {
@@ -137,7 +168,7 @@ $("#header").prepend(formattedName);
 	 "degree": "BA",
 	 "major" : "Mathematics",
 	 "dates": "May 1997",
-	 "url":"fau.edu"
+	 "url":"http://www.fau.edu/"
 	 }
 	 
 	 ],
@@ -147,21 +178,21 @@ $("#header").prepend(formattedName);
 	 "title": "JavaScript",
 	 "school": "Udacity",
 	 "dates": "May 2017",
-	 "url":"udacity.com"
+	 "url":"https://www.udacity.com/"
 	 }, 
 	 
 	 {
 	 "title": "GitHub",
 	 "school": "Udacity",
 	 "dates": "March 2017",
-	 "url":"udacity.com"
+	 "url":"https://www.udacity.com/"
 	 },
 	 
 	 {
 	 "title": "The Complete Cyber Security Course",
 	 "school": "Udemy",
 	 "dates": "in progress",
-	 "url":"udemy.com"
+	 "url":"https://www.udemy.com"
 	 }
 	 
 	 ]
@@ -172,6 +203,8 @@ $("#header").prepend(formattedName);
 $("#main").append(work["position"]);
 $("#main").append(education.name);	
 
+
+//var multilineDescription = jobs['description'].multilineDescription.join('\n');
  
  
  
